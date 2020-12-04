d04430ecc3a94dc66ec4db7bf7352ac33f3f075a
kaneyan0607 <kaneyan0607@gmail.com>
2020-12-04 23:58:00 +0900

    add:キャラクター表示

---
+++ main.js
@@ -0,0 +1,20 @@
+const SCREEN_SIZE_W = 256;
+const SCREEN_SIZE_H = 224;
+
+let can = document.getElementById("can");
+let con = can.getContext("2d");
+
+can.width = 256;
+can.height = 224;
+
+con.fillStyle = "#66AAFF";
+con.fillRect(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H);
+
+let chImg = new Image();
+chImg.src = "sprite.png";
+chImg.onload = draw;
+
+
+function draw() {
+    con.drawImage(chImg, 0, 0, 16, 32, 0, 0, 16, 32);
+}
